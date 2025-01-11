ab6fbc50b73776eac15e42af6998bc7780800709
Akash <akash.w.shah@gmail.com>
2025-01-11 17:46:39 -0500

    Add bobbing animation to Bouyancy script

--- Assets/Scripts/Bouyancy.cs
+++ Assets/Scripts/Bouyancy.cs
@@ -7,17 +7,28 @@ public class Bouyancy : MonoBehaviour
     [SerializeField]
     private float bouyancy = 1;
 
+    [Header("Bobbing Animation")]
+    [Tooltip("Amplitude of sinusoidal force driving the bobbing animation")]
+    [SerializeField]
+    private float amplitude = 2;
+    [Tooltip("Period in seconds of sinusoidal force driving the bobbing animation")]
+    [SerializeField]
+    private float period = 5;
+
     private Rigidbody rb;
     private Bounds bounds;
 
     private bool inWater = false;
     private float surface;
+    private float time;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         bounds = GetComponent<BoxCollider>().bounds;
+
+        time = 0;
     }
 
     // Update is called once per frame
@@ -33,6 +44,10 @@ public class Bouyancy : MonoBehaviour
             // Bouyancy force
             float bottom = transform.position.y - (bounds.center.y + bounds.extents.y);
             rb.AddRelativeForce(bouyancy * (surface - bottom) * Vector3.up);
+
+            // Bobbing up and down animation
+            rb.AddRelativeForce(amplitude * Mathf.Sin(2 * Mathf.PI / period * time) * Vector3.down);
+            time += Time.deltaTime;
         }
     }
 
